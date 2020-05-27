cfff5e1e101a1ce8ea67d756cf9efd8a9a5949bb
Jason Few <jason.few@duke.edu>
2020-05-27 12:57:38 -0500

    create two js functions, modalbtn and spanbtn, to open html modals

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['images/portrait.JPG', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -26,3 +26,57 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+function randomizeImage() {
+  // The images directory contains 13 images, so generate a random index between
+  // 1 and 13.
+  const imageIndex = Math.floor(Math.random() * 5) + 1;
+  const imgUrl = 'images/jason-' + imageIndex + '.jpg';
+
+  const imgElement = document.createElement('img');
+  imgElement.src = imgUrl;
+
+  const imageContainer = document.getElementById('random-image-container');
+  // Remove the previous image.
+  imageContainer.innerHTML = '';
+  imageContainer.appendChild(imgElement);
+}
+
+function modalbtn() {
+    var button = document.getElementById("Intro");
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+function modalbtn2() {
+    var button = document.getElementById("Interests");
+    var modal = document.getElementById("myModal2");
+    modal.style.display = "block";
+}
+
+function spanbtn(){
+    var span = document.getElementsByClassName("close")[0];
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+function spanbtn2(){
+    var span = document.getElementsByClassName("close")[0];
+    var modal = document.getElementById("myModal2");
+    modal.style.display = "none";
+}
+
+
+// window.onclick = function(event) {
+//   var modal = document.getElementById("myModal");
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+// window.onclick = function(event) {
+//   var modal = document.getElementById("myModal2");
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
