7dccdffd1f0d698bab1a7d808581224c3b454fbb
Jason Few <jason.few@duke.edu>
2020-06-08 22:30:35 +0000

    Add Google Map API

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -104,3 +104,10 @@ window.onclick = function(event) {
   if (event.target === viewModal) viewModal.style.display = "none";  
   if (event.target === connectModal) connectModal.style.display = "none";
 }
+
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById("map"),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
