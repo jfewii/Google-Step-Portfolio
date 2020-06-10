a67c2f538d7109d144457a09147412ca788f10ac
Jason Few <jason.few@duke.edu>
2020-06-10 22:32:42 +0000

    Move chart and map js to new files

---
+++ portfolio/src/main/webapp/map.js
@@ -0,0 +1,98 @@
+/* JS for map */
+
+const DUKE_IMG = "/images/duke1.png";
+const SJS_IMG = "/images/sjsSeal.png";
+
+function createMap() {
+  const usaMap = new google.maps.Map(document.getElementById("usa-map"), {
+    /* Coordinates are the center of the USA */  
+    center: {lat: 39.8283, lng: -98.5795},
+    zoom: 4,
+    styles: [
+      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#263c3f'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#6b9a76'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#38414e'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#212a37'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9ca5b3'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#746855'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#1f2835'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#f3d19c'}]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{color: '#2f3948'}]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#17263c'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#515c6d'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{color: '#17263c'}]
+      }
+    ]  
+  });
+  const myHighSchool = {lat: 29.7425, lng: -95.4288};
+  const myCollege = {lat: 36.0014, lng: -78.9382};
+  const highSchoolMarker = new google.maps.Marker({position: myHighSchool, map: usaMap,
+    title: "My High School", icon: SJS_IMG});
+  const CollegeMarker = new google.maps.Marker({position: myCollege, map: usaMap,
+    title: "My College", icon: DUKE_IMG});
+}
